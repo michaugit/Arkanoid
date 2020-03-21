aaf6fb62f64834d09ba7324134ce8fd95cb05eb9
michalbit <restarcik69@gmail.com>
2020-03-21 21:05:10 +0100

    Upload on GitHub :)

---
+++ Unit1.h
@@ -0,0 +1,61 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+#include <ExtCtrls.hpp>
+#include <Graphics.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+        TShape *backround;
+        TImage *ball;
+        TTimer *timer;
+        TImage *paddle;
+        TTimer *lewo;
+        TTimer *prawo;
+        TImage *Image25;
+        TImage *Image1;
+        TImage *Image2;
+        TImage *Image3;
+        TImage *Image4;
+        TImage *Image5;
+        TImage *Image6;
+        TImage *Image7;
+        TImage *Image8;
+        TImage *Image9;
+        TImage *Image10;
+        TImage *Image11;
+        TImage *Image12;
+        TImage *Image13;
+        TImage *Image14;
+        TImage *Image15;
+        TImage *Image16;
+        TImage *Image17;
+        TImage *Image18;
+        TImage *Image19;
+        TImage *Image20;
+        TImage *Image21;
+        TImage *Image22;
+        TImage *Image23;
+        TImage *Image24;
+        void __fastcall timerTimer(TObject *Sender);
+        void __fastcall FormKeyDown(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall FormKeyUp(TObject *Sender, WORD &Key,
+          TShiftState Shift);
+        void __fastcall lewoTimer(TObject *Sender);
+        void __fastcall prawoTimer(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+        __fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
